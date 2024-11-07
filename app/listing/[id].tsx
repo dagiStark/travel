c18df82a3b4335248f596fd6591c9ed50750ccaa
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-11-07 11:07:01 +0300

    Update [id].tsx

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,17 +1,60 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { useLocalSearchParams } from "expo-router";
+import listingData from "@/constants/destinations.json";
+import { ListingType } from "@/types/listingTypes";
+import { Dimensions } from "react-native";
+import { Stack } from "expo-router";
+import { Feather } from "@expo/vector-icons";
+import { colors } from "@/constants/colors";
+
+const { width } = Dimensions.get("window");
+const IMG_HEIGHT = 300;
 
 const ListingDetails = () => {
   const { id } = useLocalSearchParams();
+  const listing: ListingType = (listingData as ListingType[]).find(
+    (listing) => listing.id === id
+  );
 
   return (
-    <View>
-      <Text>ListingDetails</Text>
-    </View>
+    <>
+      <Stack.Screen
+        options={{
+          headerTransparent: true,
+          headerTitle: "",
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => {}}
+              style={{
+                backgroundColor: "rgba(255,255, 255, 0.5)",
+                borderRadius: 10,
+                padding: 4,
+              }}
+            >
+              <View
+                style={{
+                  backgroundColor: colors.white,
+                  padding: 6,
+                  borderRadius: 10,
+                }}
+              >
+                <Feather name="arrow-left" size={20} />
+              </View>
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <View>
+        <Image source={{ uri: listing.image }} style={styles.image} />
+        <Text>Hello everyone</Text>
+      </View>
+    </>
   );
 };
 
 export default ListingDetails;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {},
+});
