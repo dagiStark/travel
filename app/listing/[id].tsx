5f81310076cafb6107409d27d53175486bcd83fd
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-11-07 11:41:36 +0300

    Update [id].tsx

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,11 +1,16 @@
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
-import { useLocalSearchParams } from "expo-router";
+import { useLocalSearchParams, useRouter } from "expo-router";
 import listingData from "@/constants/destinations.json";
 import { ListingType } from "@/types/listingTypes";
 import { Dimensions } from "react-native";
 import { Stack } from "expo-router";
-import { Feather } from "@expo/vector-icons";
+import {
+  Feather,
+  FontAwesome,
+  FontAwesome5,
+  Ionicons,
+} from "@expo/vector-icons";
 import { colors } from "@/constants/colors";
 
 const { width } = Dimensions.get("window");
@@ -17,6 +22,8 @@ const ListingDetails = () => {
     (listing) => listing.id === id
   );
 
+  const router = useRouter();
+
   return (
     <>
       <Stack.Screen
@@ -25,7 +32,7 @@ const ListingDetails = () => {
           headerTitle: "",
           headerLeft: () => (
             <TouchableOpacity
-              onPress={() => {}}
+              onPress={() => router.back()}
               style={{
                 backgroundColor: "rgba(255,255, 255, 0.5)",
                 borderRadius: 10,
@@ -43,11 +50,82 @@ const ListingDetails = () => {
               </View>
             </TouchableOpacity>
           ),
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => {}}
+              style={{
+                backgroundColor: "rgba(255,255, 255, 0.5)",
+                borderRadius: 10,
+                padding: 4,
+              }}
+            >
+              <View
+                style={{
+                  backgroundColor: colors.white,
+                  padding: 6,
+                  borderRadius: 10,
+                }}
+              >
+                <Ionicons name="bookmark-outline" size={20} />
+              </View>
+            </TouchableOpacity>
+          ),
         }}
       />
-      <View>
+      <View style={styles.container}>
         <Image source={{ uri: listing.image }} style={styles.image} />
-        <Text>Hello everyone</Text>
+
+        <View style={styles.contentWrapper}>
+          <Text style={styles.listingName}>{listing.name}</Text>
+          <View style={styles.listingLocationWrapper}>
+            <FontAwesome5
+              name="map-marker-alt"
+              size={18}
+              color={colors.primaryColor}
+            />
+            <Text style={styles.listingLocationTxt}>{listing.location}</Text>
+          </View>
+
+          <View style={styles.highlightWrapper}>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <View style={styles.highlightIcon}>
+                <Ionicons name="time" size={20} color={colors.primaryColor} />
+              </View>
+              <View>
+                <Text style={styles.highlightTxt}>Duration</Text>
+                <Text style={styles.highlightTxtValue}>
+                  {listing.duration} Days
+                </Text>
+              </View>
+            </View>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <View style={styles.highlightIcon}>
+                <FontAwesome
+                  name="users"
+                  size={20}
+                  color={colors.primaryColor}
+                />
+              </View>
+              <View>
+                <Text style={styles.highlightTxt}>Persons</Text>
+                <Text style={styles.highlightTxtValue}>
+                  {listing.duration} 
+                </Text>
+              </View>
+            </View>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <View style={styles.highlightIcon}>
+                <Ionicons name="star" size={20} color={colors.primaryColor} />
+              </View>
+              <View>
+                <Text style={styles.highlightTxt}>Duration</Text>
+                <Text style={styles.highlightTxtValue}>
+                  {listing.duration} Days
+                </Text>
+              </View>
+            </View>
+          </View>
+        </View>
       </View>
     </>
   );
@@ -56,5 +134,54 @@ const ListingDetails = () => {
 export default ListingDetails;
 
 const styles = StyleSheet.create({
-  image: {},
+  image: {
+    width: width,
+    height: IMG_HEIGHT,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+  },
+  contentWrapper: {
+    padding: 20,
+  },
+  listingName: {
+    fontSize: 24,
+    fontWeight: "600",
+    color: colors.black,
+    letterSpacing: 0.5,
+  },
+  listingLocationWrapper: {
+    flexDirection: "row",
+    marginTop: 5,
+    marginBottom: 10,
+  },
+  listingLocationTxt: {
+    fontSize: 14,
+    fontWeight: "600",
+    color: colors.black,
+    marginLeft: 5,
+  },
+  highlightWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 20,
+    justifyContent: "space-between",
+  },
+  highlightIcon: {
+    backgroundColor: "#F4F4F4",
+    paddingHorizontal: 8,
+    paddingVertical: 5,
+    borderRadius: 8,
+    marginRight: 5,
+    alignItems: "center",
+  },
+  highlightTxt: {
+    fontSize: 12,
+    color: "#999",
+  },
+  highlightTxtValue: {
+    fontSize: 14,
+    fontWeight: "600",
+  },
 });
